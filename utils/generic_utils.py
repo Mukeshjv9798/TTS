3eab8a5e61bc38aec4c529164eca9e51abea5bac
Mukesh <42640085+Mukeshjv9798@users.noreply.github.com>
2020-10-04 13:03:08 +0530

    Update generic_utils.py

--- utils/generic_utils.py
+++ utils/generic_utils.py
@@ -7,7 +7,24 @@ import subprocess
 import importlib
 import numpy as np
 from collections import Counter
+import re 
+import json
 
+class AttrDict(dict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+
+def load_config(config_path):
+    config = AttrDict()
+    with open(config_path, "r") as f:
+        input_str = f.read()
+    input_str = re.sub(r'\\\n', '', input_str)
+    input_str = re.sub(r'//.*\n', '\n', input_str)
+    data = json.loads(input_str)
+    config.update(data)
+    return config
 
 def get_git_branch():
     try:
